cb8e18f7ec22896ebedaa7bf32c1ad77526db7f0
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-06 22:49:11 -0500

    feat(project): :sparkles: add product entity

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,13 @@
-export class Product {}
+export class Product {
+
+    id: number;
+    name: string;
+    description: string;
+    price: number;
+    stock: number;
+    image: string;
+    
+    constructor(partial: Partial<Product>) {
+        Object.assign(this, partial);
+    }
+}
