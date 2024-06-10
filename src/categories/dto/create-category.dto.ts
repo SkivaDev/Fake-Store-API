c7d8c5cbfb038344c57af83b81065c682b43ac20
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-10 15:03:44 -0500

    feat(project): :sparkles: add create-category-dto validation

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,17 +1,22 @@
-
-import { IsNotEmpty, IsOptional, IsString, IsUrl, MinLength } from "class-validator";
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  IsUrl,
+  MinLength,
+} from 'class-validator';
 
 export class CreateCategoryDto {
-    @IsString()
-    @MinLength(4)
-    name: string;
+  @IsString()
+  @MinLength(4)
+  name: string;
 
-    @IsString()
-    @IsOptional()
-    description: string;
+  @IsString()
+  @IsOptional()
+  description: string;
 
-    @IsString()
-    @IsNotEmpty()
-    @IsUrl()
-    image: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsUrl()
+  image: string;
 }
