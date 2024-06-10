58b18973b82b45916d75a62caf2608e34c6fa28f
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-10 16:08:15 -0500

    feat(project): :sparkles: add images

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
+import { ImagesModule } from './images/images.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule],
+  imports: [ProductsModule, CategoriesModule, ImagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
