ad906359d80c3441d08512272e469023a9656d81
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-11 11:37:26 -0500

    fix(products): :hammer: update filter products dto

--- src/products/dto/filter-products.dto.ts
+++ src/products/dto/filter-products.dto.ts
@@ -1,40 +1,47 @@
-import { Type } from "class-transformer";
-import { IsNumber, IsOptional, IsPositive, IsString, Min, ValidateIf } from "class-validator";
+import { Type } from 'class-transformer';
+import {
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  Min,
+  ValidateIf,
+} from 'class-validator';
 
 export class FilterProductsDto {
-    @IsNumber()
-    @IsOptional()
-    @Type(() => Number)
-    limit?: number;
-  
-    @IsNumber()
-    @IsOptional()
-    @Type(() => Number)
-    offset?: number;
-  
-    @IsOptional()
-    @IsNumber()
-    @Type(() => Number)
-    price?: number;
-  
-    @IsOptional()
-    @IsNumber()
-    @Min(0)
-    @Type(() => Number)
-    price_min?: number;
-  
-    @ValidateIf((params) => params.minPrice)
-    @IsPositive()
-    @Type(() => Number)
-    price_max?: number;
-  
-    @IsOptional()
-    @IsString()
-    name?: string;
-  
-    @IsOptional()
-    @IsNumber()
-    @Min(1)
-    @Type(() => Number)
-    categoryId?: number;
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  limit?: number;
+
+  @IsNumber()
+  @IsOptional()
+  @Type(() => Number)
+  offset?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Type(() => Number)
+  price?: number;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
+  @Type(() => Number)
+  price_min?: number;
+
+  @ValidateIf((params) => params.minPrice)
+  @IsPositive()
+  @Type(() => Number)
+  price_max?: number;
+
+  @IsOptional()
+  @IsString()
+  name?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @Min(1)
+  @Type(() => Number)
+  categoryId?: number;
 }
