a0dccd1b3a55d954ac5b150d11ea40af8660cdef
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-18 18:57:25 -0500

    feat(users): :sparkles: add user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,10 @@ import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 import { CategoriesModule } from './categories/categories.module';
 import { ImagesModule } from './images/images.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ProductsModule, CategoriesModule, ImagesModule],
+  imports: [ProductsModule, CategoriesModule, ImagesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
