e815e8854b3c20f6695356cb41f43058c276931e
Fabrizio Ortiz <fabrizioortizo.main@gmail.com>
2024-06-20 18:17:17 -0500

    feat(users): :sparkles: add Validation for create user dto

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,7 +1,27 @@
+import { IsEmail, IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, Matches, MinLength } from "class-validator";
+
 export class CreateUserDto {
+    @IsString()
+    @MinLength(3)
     name: string;
+
+    @IsString()
+    @IsEmail()
+    @IsNotEmpty()
     email: string;
+
+    @IsString()
+    @MinLength(6)
     password: string;
+
+    @IsString()
+    @IsEnum(['admin', 'user'])
+    @IsOptional()
     role?: string;
+
+    @IsString()
+    @IsUrl({protocols: ['http', 'https']})
+    @Matches(/(https?:\/\/.*\.(?:png|jpg))/, { message: 'Invalid image url'})
+    @IsOptional()
     avatar?: string;
 }
